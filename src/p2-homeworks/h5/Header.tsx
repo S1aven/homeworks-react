0a791174f9d8ddcd4978306461dc332adc6efd2e
Sergei <rudnev09@mail.ru>
2022-03-06 20:23:34 +0300

    homework_5-1

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Routes";
+import s from './Header.module.css'
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
-
+  return (
+    <div className={s.navbar}>
+      <div className={s.menu}>
+        {/*// add NavLinks*/}
+        <div className={s.item}>
+          <NavLink className={({isActive}) => isActive ? s.active : ''} to={PATH.PRE_JUNIOR}>PreJunior</NavLink>
         </div>
-    )
+        <div className={s.item}>
+          <NavLink className={({isActive}) => isActive ? s.active : ''} to={PATH.JUNIOR}>Junior</NavLink>
+        </div>
+        <div className={s.item}>
+          <NavLink className={({isActive}) => isActive ? s.active : ''} to={PATH.JUNIOR_PLUS}>Junior+</NavLink>
+        </div>
+      </div>
+    </div>
+
+  )
 }
 
 export default Header
---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const JuniorPlus = () => {
+  return (
+    <div>
+      junior +
+    </div>
+  );
+};
+
+export default JuniorPlus;
